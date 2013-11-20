a7c6b0f428baf1843630014eb70c234ff67d0b63
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 17:02:28 -0600

    added post controller and post view files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,27 @@
-class PostsController < ApplicationController 
+class PostsController < ApplicationController
+
+  def index
+    # show all posts
+    @posts = Post.all
+  end
+
+  def new
+    #form for creating a new post
+
+  end
+
+  def create
+    #create a new post
+    @new_post = Post.create(params[:post])
+
+  end
+
+  def show
+    #show a specific post
+    #/
+    @post = Post.find_by_id(params[:id])
+
+  end
+
 
 end
