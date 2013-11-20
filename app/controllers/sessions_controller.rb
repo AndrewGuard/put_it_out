41042660501b1b338a4ce733514af068683bdc2a
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 17:14:11 -0600

    create user form, login user form, user validations for email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,20 @@
 class SessionsController < ApplicationController 
 
+	def create
+		user = params[:session][:email]
+		if user
+			if user.authenticate
+			session[:id] = user.id
+			redirect_to user_path(user)
+		end
+		flash[:error] = "You entered the wrong password, buddy"
+
+		else
+			flash[:error] = "email is not found in our system"
+		end
+	end
+
+	def logout
+	end
+
 end
