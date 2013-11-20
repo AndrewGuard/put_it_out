a7e709a77f542610aaa0a8f77e64fbc175ed0222
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-11-20 16:57:43 -0600

    Added new method and new view

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -7,4 +7,20 @@ class ResourcesController < ApplicationController
 		@resources = Resource.all
 	end
 
+	def new
+	  @resources= Resource.new
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def update
+	end
+
+	def create
+	end
+
 end
