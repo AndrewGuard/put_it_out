f2ab768cc55e198ce8811537237d4a9188aa497f
Apprentice <apprentice@dbc-chicago-004.local>
2013-11-21 09:01:46 -0600

    Added validations to posts and Rspec test to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ActiveRecord::Base
    attr_accessible :title, :body
 
    validates :title, :body, presence: true
-
+   validates :title, length: { in: 4..300}
+   validates :body, length: { minimum: 4}
    belongs_to :user
    has_many :comments
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:post) {Post.new(title: "This is the musky title", body: "Musk is lovely and awesome", )}
+
+
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:body) }
+  it { should ensure_length_of(:body).is_at_least(4) }
+  it { should ensure_length_of(:title).is_at_most(300) }
+  it { should ensure_length_of(:title).is_at_least(4) }
+
 end
+
