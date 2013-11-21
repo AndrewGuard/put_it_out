77c361ed2e5bb4a9040ef83c07dbce853c06f2d3
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-11-20 18:10:09 -0600

    added tests for title presence and url validation

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
-describe Resource do
-  pending "add some examples to (or delete) #{__FILE__}"
+	describe Resource do
+	let (:resource) {Resource.new}
+
+	context "when title is empty" do
+		it "should not be valid" do
+			expect(user.valid?).to be_false
+		end
+
+		it "should not save" do
+			expect(resource.save).to be_false
+		end
+	end
+
+	context "when url is invalid" do
+		let (:resource) {Resource.new(:link => hte.get)}
+
+			it "should not save" do
+				expect(resource.save).to be_false
+			end
+	end
 end
