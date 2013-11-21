0de47b507c5c4c6e83160f3d6b8b3fd0745a60b7
Apprentice <apprentice@dbc-chicago-004.local>
2013-11-21 17:02:41 -0600

    add lots of comment stuff

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,21 @@
 class CommentsController < ApplicationController
-    def new
-      comment = Comment.new
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment= Comment.(params[:comment])
+
+      if @comment
+        redirect_to :back
+      else
+        flash[:error] = "Your comment is uncool"
+      end
+  end
+
+    def show
+      @comment = Comment.find(params[:id])
+      respond_with @comment
     end
-
-    def create
-
-    end
-
-
-
 end
+
