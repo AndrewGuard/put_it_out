a2f555ec3207e6b89f8553c13889e3f66ceb4050
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-20 19:48:42 -0600

    tests for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ActiveRecord::Base
-   attr_accessible :name, :email, :password, :username
+	attr_accessible :name, :email, :password, :username
 
-   has_many :posts
-   has_many :comments
+	has_many :posts
+	has_many :comments
 
-  validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
-  
+	validates_presence_of :password, :on => :create
+	validates :password, length: { in: 6..20, message: "Must be between 6 and 20 characters" }
+	validates_presence_
+	validates_uniqueness_of :email
+	validates :email, format: {with: /[a-zA-Z]+@.{2,}\.[a-zA-Z]{2,6}/, message: "Enter an email that works"}
+	validates_presence_of :username
+	validates_uniqueness_of :username
+	validates :username, length: { in: 6..20, message: "Must be between 6 and 20 characters" }
+	validates_presence_of :name
+	validates :name, length: { in: 2..20, message: "Must be between 2 and 20 characters" }
 
 
-   has_secure_password
+	has_secure_password
 end
