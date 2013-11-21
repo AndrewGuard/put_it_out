8584bdab45b83860717a8f3e2904a7bea4ce44fb
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-21 16:13:55 -0600

    added resources

--- spec/features/navbar_links_spec.rb
+++ spec/features/navbar_links_spec.rb
@@ -4,15 +4,15 @@
 require 'spec_helper'
 
 feature "navbar links work" do
-  scenario "click login" do
-    visit '/'                        # visit homepage
-    click_link "Login"               # click login
-    current_path.should == new_user_path    # verify link
-  end
+  # scenario "click login" do
+  #   visit '/'                        # visit homepage
+  #   click_link "Sign Up or Login"               # click login
+  #   current_path.should == new_user_path    # verify link
+  # end
 
-  scenario "click sign up" do
+  scenario "click sign up or login" do
     visit '/'                        # visit homepage
-    click_link "Sign Up"               # click sign up
+    click_link "Sign Up or Login"               # click sign up or login
     current_path.should == new_user_path    # verify link
   end
 
@@ -22,10 +22,16 @@ feature "navbar links work" do
     current_path.should == '/'    # verify link
   end
 
-  # Something wrong
+  # Fix this
+  #  scenario "click resources" do
+  #   visit '/'                        # visit homepage
+  #   click_link "Resources"               # click resources
+  #   current_path.should == resource_path    # verify link
+  # end
+
   scenario "click logout" do
     visit '/'                        # visit page where sessions are in session
-    click_link "Sign Up" 
+    click_link "Sign Up or Login" 
     fill_in "Name", :with => "Test Person"
     # first(:email, "Email").fill_in, :with => "test_person@mail.com" 
     fill_in "user_email", :with => "test_person@mail.com"
@@ -39,7 +45,7 @@ feature "navbar links work" do
 
   scenario "click create new post" do
     visit '/'                        # visit page where sessions are in session
-    click_link "Sign Up" 
+    click_link "Sign Up or Login" 
     fill_in "Name", :with => "Test Person"
     # first(:email, "Email").fill_in, :with => "test_person@mail.com" 
     fill_in "user_email", :with => "test_person@mail.com"
