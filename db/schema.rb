619c8535332f7755725f0c272b27f9b692090c8a
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-21 15:17:06 -0600

    index view for user and changed posts table in db to have a text type for body field

--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(:version => 20131120203904) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
