def1cd499e34e89a061cf03af24a5d693a80c36d
Andrew Guard <andrew.s.guard@gmail.com>
2013-11-22 15:35:41 -0600

    fixed comment link to show the user who created the comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
-
 class PostsController < ApplicationController
   include ApplicationHelper
 
   def index
     # show all posts
     @posts = Post.all
+  end
+
+  def show
+    #show a specific post
+    @post = Post.find_by_id(params[:id])
   end
 
   def new
@@ -22,11 +26,29 @@ class PostsController < ApplicationController
     #   flash[:error] = "Enter a title and body"
       render :new
     end
-
   end
 
-  def show
-    #show a specific post
-    @post = Post.find_by_id(params[:id])
+  def update
+    @post = Post.find(params[:id])
+
+    respond_to do |format|
+      if @post.update_attributes(params[:post])
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.json { head :no_content }
+    end
   end
 end
