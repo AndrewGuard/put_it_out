8932dc69b23c8d23e0136e713ccd454404c8bd41
dbc-apprentice <apprentice@devbootcamp.com>
2013-11-22 16:48:17 -0600

    saving

--- spec/features/user_log_in_with_facebook_spec.rb
+++ spec/features/user_log_in_with_facebook_spec.rb
@@ -4,10 +4,11 @@ feature "registered user can login with facebook" do
 	scenario "for a particular user" do
 		visit "/users/new"
 		click_button "fbook_button2"
+		
 		expect(page).to have_text("Facebook Login")
 		fill_in 'email', :with=> 'jvvchapman@gmail.com'
 		fill_in 'pass', :with =>'p@ssword'
-		# click_button "login_button"
-		# expect(page).to redirect_to()
+		click_button "login_button"
+		expect(page).to redirect_to()
 	end
 end
