dc1466751211c6263dba0a1c95e7ac834285859e
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2013-11-23 17:42:34 -0600

    helper methods for timers

--- app/helpers/timer_helper.rb
+++ app/helpers/timer_helper.rb
@@ -1,3 +1,4 @@
+require 'time'
 module TimerHelper 
 	
 	def current_timer(user_id)
@@ -6,7 +7,26 @@ module TimerHelper
 	end
 
 	def time_since_last_cig(last_user_cig)
+	   exact_time =(Time.now - last_user_cig)
+		self.time_broken_down(exact_time)
 	end
 
+	def time_broken_down_seconds(exact_time)
+		 exact_time*60
+	end
 
+	def display_days(seconds)
+		seconds/86400
+	end
+
+	def display_hours(seconds)
+		seconds % 86400 / 3600
+	end
+
+	def display_minute(seconds)
+		seconds % 86600 % 3600 / 60
+	end
+
+	def display_seconds(seconds)
+		seconds % 86600 % 3600 % 60 
 	end
